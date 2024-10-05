3dc545955634d6f5eff5b269c3996119fd2215b6
ALPHA-KISHORE <balachanderkishore@gmail.com>
2024-10-05 10:37:04 +0530

    Update app.py
    after backend ssl certifcate issue

--- app.py
+++ app.py
@@ -14,23 +14,28 @@ API_KEY = '776b909054d194e0098441aa4eec7993f1d091af'
 
 def get_air_quality_data(city):
     url = f"https://api.waqi.info/feed/{city}/?token={API_KEY}"
-    response = requests.get(url)
-    data = response.json()
+    try:
+        response = requests.get(url, verify=False)  # Ignore SSL certificate warnings
+        data = response.json()
 
-    if data['status'] == 'ok':
-        air_quality_data = {
-            'city': data['data']['city']['name'].split('(')[0].strip(),  # Removes the Hindi part
-            'aqi': data['data']['aqi'],
-            'pollutants': {
-                'Humidity': data['data']['iaqi'].get('h', {'v': 'N/A'})['v'],
-                'Pressure': data['data']['iaqi'].get('p', {'v': 'N/A'})['v'],
-                'PM2.5': data['data']['iaqi'].get('pm25', {'v': 'N/A'})['v'],
-                'Temperature': data['data']['iaqi'].get('t', {'v': 'N/A'})['v'],
-                'Wind Speed': data['data']['iaqi'].get('w', {'v': 'N/A'})['v'],
-                'Wind Gust': data['data']['iaqi'].get('wg', {'v': 'N/A'})['v'],
+        if data['status'] == 'ok':
+            air_quality_data = {
+                'city': data['data']['city']['name'].split('(')[0].strip(),  # Removes the Hindi part
+                'aqi': data['data']['aqi'],
+                'pollutants': {
+                    'Humidity': data['data']['iaqi'].get('h', {'v': 'N/A'})['v'],
+                    'Pressure': data['data']['iaqi'].get('p', {'v': 'N/A'})['v'],
+                    'PM2.5': data['data']['iaqi'].get('pm25', {'v': 'N/A'})['v'],
+                    'Temperature': data['data']['iaqi'].get('t', {'v': 'N/A'})['v'],
+                    'Wind Speed': data['data']['iaqi'].get('w', {'v': 'N/A'})['v'],
+                    'Wind Gust': data['data']['iaqi'].get('wg', {'v': 'N/A'})['v'],
+                }
             }
-        }
-    else:
+        else:
+            air_quality_data = None
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
         air_quality_data = None
 
     return air_quality_data
@@ -46,3 +51,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
